a280ac312dd9e0747cee1899e9c536da9a657257
Davinson Castaneda <davinsonbmx@gmail.com>
2023-07-24 22:47:42 -0500

    Home page added

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+
+export default () => (
+<div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="container secondary-color">
+        <h1 className="display-4">Bulk dates assignments</h1>
+        <p className="lead">
+          Schedules
+        </p>
+        <hr className="my-4" />
+        <div className="d-flex justify-content-between">
+          <Link
+            to="/schedules"
+            className="btn btn-lg custom-button "
+            role="button"
+          >
+            Create Schedules
+          </Link>
+          <Link
+          to="/schedules"
+          className="btn btn-lg custom-button ml-4"
+          role="button"
+        >
+          View Schedules
+        </Link>
+        </div>
+      </div>
+    </div>
+  </div>
+);
